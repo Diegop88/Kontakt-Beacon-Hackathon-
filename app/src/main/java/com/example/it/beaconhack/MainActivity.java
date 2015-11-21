f5df4139fe8daa9055f9ba2e69978d062100abf7
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2015-11-21 14:48:42 -0600

    Fixing a dependency conflict between two libs from kontakt.

--- app/src/main/java/com/example/it/beaconhack/MainActivity.java
+++ app/src/main/java/com/example/it/beaconhack/MainActivity.java
@@ -5,18 +5,13 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Toast.makeText(getApplicationContext(), "HOla", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), "ADIOS", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Hola", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Adios", Toast.LENGTH_SHORT).show();
         Toast.makeText(getApplicationContext(), "Cambio", Toast.LENGTH_SHORT).show();
-
-        //TO DO: HOLA
-
-
     }
 }
