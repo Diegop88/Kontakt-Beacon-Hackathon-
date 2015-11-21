5f1dc0da0762dee9c6134ecec5eb45448faf3aad
Diego Alfredo Ramirez Vasquez <diego.ramirez@schibsted.com.mx>
2015-11-21 10:25:01 -0600

    SDK setup

---
+++ app/src/main/java/com/example/it/beaconhack/Fragmento.java
@@ -0,0 +1,14 @@
+package com.example.it.beaconhack;
+
+import android.support.v4.app.Fragment;
+import android.widget.TextView;
+
+/**
+ * Created by diego on 20/11/15.
+ *
+ */
+public class Fragmento extends Fragment {
+
+    TextView textView;
+
+}
--- app/src/main/java/com/example/it/beaconhack/MainActivity.java
+++ app/src/main/java/com/example/it/beaconhack/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.it.beaconhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Toast.makeText(getApplicationContext(), "HOla", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "ADIOS", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Cambio", Toast.LENGTH_SHORT).show();
+
+        //TO DO: HOLA
+
+
     }
 }
