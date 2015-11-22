02e558c035a814de74bde6fe2afee25a40c7279d
José Luis Cadena <joseluis.cadena@schibsted.com.mx>
2015-11-22 03:17:06 -0600

    TapPoint Implementation

--- app/src/main/java/com/example/it/beaconhack/Home.java
+++ app/src/main/java/com/example/it/beaconhack/Home.java
@@ -1,13 +1,14 @@
 package com.example.it.beaconhack;
 
-import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,31 +25,76 @@ import com.kontakt.sdk.android.ble.discovery.ibeacon.IBeaconDeviceEvent;
 import com.kontakt.sdk.android.ble.filter.ibeacon.IBeaconFilter;
 import com.kontakt.sdk.android.ble.filter.ibeacon.IBeaconFilters;
 import com.kontakt.sdk.android.ble.manager.ProximityManager;
-import com.kontakt.sdk.android.ble.rssi.RssiCalculators;
 import com.kontakt.sdk.android.common.profile.IBeaconDevice;
+import com.proxama.tappoint.auth.AuthListener;
+import com.proxama.tappoint.auth.Authentication;
+import com.proxama.tappoint.error.ApiError;
+import com.proxama.tappoint.reporting.Reporting;
+import com.proxama.tappoint.reporting.TriggerEventType;
+import com.proxama.tappoint.sync.SyncListener;
+import com.proxama.tappoint.sync.SyncResult;
+import com.proxama.tappoint.sync.Synchronisation;
+import com.proxama.tappoint.trigger.Trigger;
+import com.proxama.tappoint.trigger.Triggers;
 
-import org.w3c.dom.Text;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by diego on 21/11/15.
  */
-public class Home extends AppCompatActivity implements ProximityManager.ProximityListener {
+public class Home extends AppCompatActivity implements ProximityManager.ProximityListener, AuthListener, SyncListener,
+        BeaconEventClickListener {
 
     private ProximityManager proximityManager;
     private ScanContext scanContext;
-
+    /** Log tag. */
+    private static final String TAG = Home.class.getSimpleName();
     TextView textView;
     TextView listDevices;
+
+    /** Name of the app as recognised by the TapPoint server. */
+    private static final String APP_NAME = "khbushwacker";
+
+    /** JSON key for the data block found within the payload of a trigger. */
+    public static final String JSON_KEY_DATA = "data";
+
+    /** JSON key for the subtitle found within the payload of a trigger. */
+    public static final String JSON_KEY_SUBTITLE = "subtitle";
+
+    /** JSON key for the title found within the payload of a trigger. */
+    public static final String JSON_KEY_TITLE = "title";
+
+    /** JSON key for the title found within the payload of a trigger. */
+    public static final String JSON_KEY_ID = "id";
+
+    /** JSON key for the title found within the payload of a trigger. */
+    public static final String JSON_KEY_IMAGE = "image";
+
+    ProgressBar mProgressBar;
+
+    /** Broadcast receiver to receive beacon events. */
+    private BeaconEventReceiver mBroadcastReceiver;
+
+    /** Intent filter to listen to the specific beacon event intent. */
+    private IntentFilter mBeaconEventFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.pbProgressIndicator);
+
+        Authentication.getAuthManager(this).authenticate(APP_NAME, this);
+
+//        configureBeaconEventList();
+        configureBeaconEventListener();
 
         textView = (TextView) findViewById(R.id.textinfo);
         listDevices = (TextView) findViewById(R.id.listdevices);
@@ -160,4 +206,114 @@ public class Home extends AppCompatActivity implements ProximityManager.Proximit
             default:
         }
     }
+
+    @Override
+    public void onAuthSuccess() {
+        Toast.makeText(this, "Auth successful", Toast.LENGTH_LONG).show();
+
+        Synchronisation.getSyncManager(this).synchronise(this);
+    }
+
+    @Override
+    public void onAuthFailure(ApiError apiError) {
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        Toast.makeText(this, "Auth failed: " + apiError.name(), Toast.LENGTH_LONG).show();
+        Log.d(TAG, apiError.getErrorMessage());
+    }
+
+    @Override
+    public void onSyncSuccess(SyncResult syncResult) {
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        int numberAdded = syncResult.getTriggersAdded().size();
+        int numberRemoved = syncResult.getTriggersRemoved().size();
+
+        Toast.makeText(this, "Sync successful. Added triggers: " + numberAdded + ". Removed triggers: " +
+                numberRemoved, Toast.LENGTH_LONG).show();
+        Triggers.getTriggersManager(this).startMonitoring();
+    }
+
+    @Override
+    public void onSyncFailure(ApiError apiError) {
+        mProgressBar.setVisibility(View.INVISIBLE);
+
+        Toast.makeText(this, "Sync failed: " + apiError.name(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onBeaconEventClicked(String beaconId) {
+        Reporting.getReportingManager(this).reportTriggerEvent(TriggerEventType.USER_SELECTED, beaconId);
+    }
+
+    /**
+     * Configures the beacon event listener that will listen for beacon events.
+     */
+    private void configureBeaconEventListener() {
+        mBroadcastReceiver = new BeaconEventReceiver();
+        mBeaconEventFilter = new IntentFilter(Triggers.ACTION_TRIGGERS_DETECTED);
+    }
+
+
+
+
+    /**
+     * {@link android.content.BroadcastReceiver} to listen to beacon events.
+     */
+    private class BeaconEventReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("intent.getAction()","-->"+intent.getAction());
+            if (Triggers.ACTION_TRIGGERS_DETECTED.equals(intent.getAction())) {
+                ArrayList<Trigger> triggers = intent.getParcelableArrayListExtra(Triggers.EXTRA_DETECTED_TRIGGERS);
+                addBeaconEvents(triggers);
+            }
+        }
+    }
+
+    /**
+     * Adds all beacon events that have recently been raised as a result of monitoring for beacon triggers.
+     *
+     * @param beacons a list of beacons to add as beacon events.
+     */
+    private void addBeaconEvents(ArrayList<Trigger> beacons) {
+        for (Trigger beacon : beacons) {
+            try {
+//                mBeaconAdapter.addEvent(getBeaconEvent(beacon));
+
+                Reporting.getReportingManager(this).reportTriggerEvent(TriggerEventType.USER_NOTIFIED,
+                        beacon.getTriggerId());
+            } catch (Exception e) {
+                Log.d(TAG, "Could not parse JSON from beacon.");
+            }
+        }
+    }
+
+    /**
+     * Gets the {@link com.proxama.tpsdkreferenceapp.BeaconEvent} according to the given beacon.
+     *
+     * @param beacon the beacon to retrieve trigger data from.
+     * @return the converted beacon event to display.
+     */
+//    private BeaconEvent getBeaconEvent(Trigger beacon) throws JSONException {
+//        String triggerId = beacon.getTriggerId();
+//        JSONObject triggerData = beacon.getTriggerPayload().getJSONObject(JSON_KEY_DATA);
+//        String subtitle = triggerData.getString(JSON_KEY_SUBTITLE);
+//        String title = triggerData.getString(JSON_KEY_TITLE);
+//
+//        return new BeaconEvent(triggerId, title, subtitle, getBeaconImageId(title));
+//    }
+
+    /**
+     * Returns the beacon image ID given the beacon's title.
+     *
+     * @param title to use for looking up the correct image resource.
+     * @return the image ID used as an image resource.
+     */
+//    private int getBeaconImageId(String title) {
+//        Integer imageId = mImageMap.get(title);
+//        return imageId == null ? R.drawable.image_1 : imageId;
+//    }
+
 }
